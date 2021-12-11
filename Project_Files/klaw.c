fd7f09a1000ac1c0cedd5ae1ec544fc12674846c
mariuszwieclawek <mariusz.wieclawek99@wp.pl>
2021-12-11 22:18:45 +0100

    Keyboard 4x4 tact switches init and read values

--- Project_Files/klaw.c
+++ Project_Files/klaw.c
@@ -1,28 +1,27 @@
 #include "klaw.h"
 
-
 void Klaw_Init(void)
 {
-	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;		// Wlaczenie portu A
-	/* Kolumny jako wejscia */
+	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;		// Enable port A
+	/* Columns as inputs */
 	PORTA->PCR[C1] |= PORT_PCR_MUX(1);
 	PORTA->PCR[C2] |= PORT_PCR_MUX(1);
 	PORTA->PCR[C3] |= PORT_PCR_MUX(1);
 	PORTA->PCR[C4] |= PORT_PCR_MUX(1);
-	PORTA->PCR[C1] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK; //Bit PE wlacza mozliwosc uzycia rezystora dodatkowego, a bit PS wybiera, jak ten rezystor ma byc podlaczony.
+	PORTA->PCR[C1] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK; //The PE bit enables the use of a secondary resistor, and the PS bit selects how this resistor is to be connected.
 	PORTA->PCR[C2] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	PORTA->PCR[C3] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	PORTA->PCR[C4] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	
-	/* Wiersze jako wyjscia */
-	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;		// Wlaczenie portu B
+	/* Rows as outputs */
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;		// Enable port B
 	PORTB->PCR[R1] |= PORT_PCR_MUX(1);
 	PORTB->PCR[R2] |= PORT_PCR_MUX(1);
 	PORTB->PCR[R3] |= PORT_PCR_MUX(1);
 	PORTB->PCR[R4] |= PORT_PCR_MUX(1);
-	PTB->PDDR |= R1_MASK|R2_MASK|R3_MASK|R4_MASK;	// Ustaw na 1 bity 6, 7 , 11, 13 – rola jako wyjscia
-	PTB->PDOR |= R1_MASK|R2_MASK|R3_MASK;	// Poczatkowo trzy wyjscia ustawiana sa na stan wysoki napiecia.
-	PTB->PDOR &= ~R4_MASK;   // Pierwszy wiersz na stan niski 
+	PTB->PDDR |= R1_MASK|R2_MASK|R3_MASK|R4_MASK;	// Set to 1 bits 6, 7, 11, 13 - role as output
+	PTB->PDOR |= R1_MASK|R2_MASK|R3_MASK;	// Initially, the three outputs are set to high voltage.
+	PTB->PDOR &= ~R4_MASK;   // First row set to low voltage
 }
 
 unsigned int read_keypad(void) 
@@ -31,21 +30,20 @@ unsigned int read_keypad(void)
 	
 	for(row = 0x1fff, key = 1; row != 0x3fdf; row = (row >> 1)|(1<<13))
 	{
-		if(row == 0x2fff) //Pomijamy PTB12
+		if(row == 0x2fff) // Omit PTB12
 			continue;
-		else if(row == 0x3bff) //Pomijamy PTB10
+		else if(row == 0x3bff) // Omit PTB10
 			continue;
-		else if(row == 0x3dff) //Pomijamy PTB9
+		else if(row == 0x3dff) // Omit PTB9
 			continue;
-		else if(row == 0x3eff) //Pomijamy PTB8
+		else if(row == 0x3eff) // Omit PTB8
 			continue;
 		PTB->PDOR = row ;
 		for(col=0x200; col!=0x2000; col<<=1, key++)
 		{
 			if(!(PTA->PDIR & col)) 
 				return key;
-		}
-			
+		}	
 	}
 	return 0;
 }
--- Project_Files/klaw.h
+++ Project_Files/klaw.h
@@ -1,26 +1,36 @@
+/*
+Keyboard 4x4 tact switches init and function to read which switch is pushed
+*/
+
+#ifndef KLAW_H
+#define KLAW_H
+
+
 #include "MKL05Z4.h"
 
-#define C1 9		// Kolumna pierwsza zestawu 4x4 tact switches
+#define C1 9		// first column 4x4 tact switches
 #define C2 10
 #define C3 11
 #define C4 12
 
-#define R1 6		// Wiersz pierwszy zestawu 4x4 tact switches
+#define R1 6		// first row 4x4 tact switches
 #define R2 7
 #define R3 11
 #define R4 13
 
 
-#define C1_MASK	(1<<9)		// Maska dla kolumny C4
-#define C2_MASK	(1<<10)		// Maska dla kolumny C3
-#define C3_MASK	(1<<11)		// Maska dla kolumny C2
-#define C4_MASK	(1<<12)		// Maska dla kolumny C1
+#define C1_MASK	(1<<9)		// Mask for C4 column
+#define C2_MASK	(1<<10)		
+#define C3_MASK	(1<<11)		
+#define C4_MASK	(1<<12)		
 
-#define R1_MASK	(1<<6)		// Maska dla wiersza R1
-#define R2_MASK	(1<<7)		// Maska dla wiersza R2
-#define R3_MASK	(1<<11)		// Maska dla wiersza R3
-#define R4_MASK	(1<<13)		// Maska dla wiersza R4
+#define R1_MASK	(1<<6)		// Mask for R1 row
+#define R2_MASK	(1<<7)		
+#define R3_MASK	(1<<11)		
+#define R4_MASK	(1<<13)		
 
 
-void Klaw_Init(void);		// Funkcja inicjujaca porty do pracy
-unsigned int read_keypad(void);		//Funkcja zwraca wartosci wcisnietego klawisza zgodnie z kolejnoscia 1-16. Dla S1 return 1; Dla S8 return 8;
+void Klaw_Init(void);		// Ports init
+unsigned int read_keypad(void);		//The function returns the values ??of the pressed key in the order 1-16. For S1 return 1; For S8 return 8;
+
+#endif
